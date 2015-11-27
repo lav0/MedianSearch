bc6e3edf5dcaee09bd8679210f3587c6795497f9
lav0 <andreigenuine@yahoo.com>
2015-11-27 18:15:51 +0800

    Compare three methods of median search: 1. with sort 2. from numpy 3. from statistics library

--- median_engine.py
+++ median_engine.py
@@ -1,7 +1,9 @@
 import numpy
 import statistics
 import random
+import time
 from TimeMeasureDecorator import time_measure_decorator
+import matplotlib.pyplot as plt
 
 
 @time_measure_decorator
@@ -25,13 +27,55 @@ def median_stat(lst):
 
 def generate_list_of_randoms(count):
     ret = list()
-    for i in range(count):
+    i = 0
+    while i != count:
         ret.append(random.uniform(-1000, 1000))
+        i = i + 1
     return ret
 
 
-lst = generate_list_of_randoms(1000000)
 
-print median_with_sort(lst)
-print median_numpy(lst)
-print median_stat(lst)
+fig = plt.gcf()
+#fig.add_subplot(111, aspect='equal')
+
+#line = plt.Line2D((.1, .9), (.1, .9), marker='o', color='r')
+
+#lst = generate_list_of_randoms(10 ** 7)
+first = None
+second = None
+
+indices = numpy.arange(0.0, 25.0)
+values_sort = [0]
+values_numpy = [0]
+values_stat = [0]
+for i in indices:
+    if i == 0.0:
+        continue
+    count = 2 ** i
+    lst = generate_list_of_randoms(count)
+    time_before = time.time()
+    result_sort = median_with_sort(lst)
+    time_after = time.time()
+    values_sort.append(10 * (time_after - time_before))
+    time_before = time.time()
+    result_numpy = median_numpy(lst)
+    time_after = time.time()
+    values_numpy.append(10 * (time_after - time_before))
+    time_before = time.time()
+    result_stat = median_stat(lst)
+    time_after = time.time()
+    values_stat.append(10 * (time_after - time_before))
+    if (result_sort != result_numpy or result_sort != result_stat):
+        print result_sort
+        print result_numpy
+        print result_stat
+
+plt.plot(indices, values_sort, 'r--')
+plt.plot(indices, values_numpy, 'g--')
+plt.plot(indices, values_stat, 'b--')
+
+# print median_with_sort(lst)
+# print median_numpy(lst)
+# print median_stat(lst)
+
+plt.show()
